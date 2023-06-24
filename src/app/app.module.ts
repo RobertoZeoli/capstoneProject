4aab89e5cc9e514a0c8275cf2405874b90ce0ce7
RobertoZeoli <127244054+RobertoZeoli@users.noreply.github.com>
2023-06-24 09:22:28 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,67 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { Error404Component } from './components/error404/error404.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { CompletedComponent } from './components/completed/completed.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+const route: Route[]=[
+  {
+    path:'',
+    redirectTo: 'home',
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component: HomeComponent,
+  },
+  {
+    path:'todo',
+    component: TodoComponent,
+  },
+  {
+    path:'login',
+    component: LoginComponent,
+    children:[
+      {
+        path:'register',
+        component: RegisterComponent,
+      },
+    ]
+  },
+  {
+    path:'register',
+    component: RegisterComponent,
+  },
+  {
+    path:'**',
+    component: Error404Component,
+  },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    WelcomeComponent,
+    HomeComponent,
+    RegisterComponent,
+    Error404Component,
+    TodoComponent,
+    CompletedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  backgroundImages: string[]=[
+    'https://cdn.travelpulse.com/images/31aaedf4-a957-df11-b491-006073e71405/bb73aab8-f2bf-4278-88c3-33dd628df23f/630x355.jpg',
+    'https://i0.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/03/Positano-Weather.jpg?fit=1600%2C1066&ssl=1',
+    'https://www.unitremilano.it/wp-content/uploads/2019/04/Il-Duomo-di-Milano.jpg',
+    'https://sofitel.accor.com/destinations/imagerie/florence-city-photo-1400x787-877d_1400x788.jpg',
+    'https://images.musement.com/cover/0002/45/turin-jpg_header-144980.jpeg',
+    'https://s27363.pcdn.co/wp-content/uploads/2022/09/Lago-di-Braies-Dolomites.jpg.optimal.jpg',
+    'https://www.sicilia.info/wp-content/uploads/sites/91/agrigento-valle-dei-templi.jpg',
+    'https://www.fulltravel.it/wp-content/uploads/2018/11/Venezia-Canale-Grande.jpg'
+  ];
+
+  currentImageIndex = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setInterval(()=>{
+      this.changeBackground();
+    },4000)
+  }
+
+  changeBackground(){
+    const randomIndex = Math.floor(Math.random() * this.backgroundImages.length);
+    this.currentImageIndex = randomIndex;
+  }
+}
