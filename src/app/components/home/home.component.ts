2a0ec16ce2842660dc80c26b1215c48d26e933a4
RobertoZeoli <127244054+RobertoZeoli@users.noreply.github.com>
2023-06-29 15:54:36 +0200

    up

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  backgroundBanner!: string;
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.changeBackgroundBanner();
   }
+
+  changeBackgroundBanner(){
+    const listaPubblicita = [
+        'url("https://thealgoristsblob.blob.core.windows.net/thealgoristsimages/stackoverflow.png")',
+        'url("https://miro.medium.com/v2/resize:fit:1358/0*wuNf24urnMp7ypDp.png")',
+        'url("https://wordpress.startsteps.org/wp-content/uploads/2022/10/EPICODE_logo.png")'
+      ];
+
+    const randomBanner = Math.floor(Math.random()*listaPubblicita.length);
+    this.backgroundBanner = listaPubblicita[randomBanner];
+
+    setTimeout(() => {
+      this.changeBackgroundBanner();
+    }, 3000);
+  }
+
+
 
 }
